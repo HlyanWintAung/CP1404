1c2ed01830abdd7c10befd7ef215f7e1ab2e69f6
HlyanWintAung <hlyanwintaung@my.jcu.edu.au>
2024-06-08 18:12:23 +0800

    write min length and length and special_characters def main and print all the require and write vaild or invalid password and do another function vaild password and write for the output

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -10,3 +10,37 @@ def main():
     print("\t1 or more uppercase characters")
     print("\t1 or more lowercase characters")
     print("\t1 or more numbers")
+    if IS_SPECIAL_CHARACTER_REQUIRED:
+        print("\tand 1 or more special characters:", SPECIAL_CHARACTERS)
+    password = input("Enter password: ")
+    while not valid_password(password):
+        print("Invalid password!")
+        password = input("Enter password: ")
+    print(f"Your {len(password)}-character password is valid: {password}")
+
+
+def valid_password(password):
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
+
+    number_of_lower = 0
+    number_of_upper = 0
+    number_of_digit = 0
+    number_of_special = 0
+    for character in password:
+        if character.isdigit():
+            number_of_digit += 1
+        elif character.islower():
+            number_of_lower += 1
+        elif character.isupper():
+            number_of_upper += 1
+        elif character in SPECIAL_CHARACTERS:
+            number_of_special += 1
+    if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0:
+        return False
+    if IS_SPECIAL_CHARACTER_REQUIRED and number_of_special == 0:
+        return False
+    return True
+
+
+main()
