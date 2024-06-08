6b093ab7c7a6341da916369f269c2d3ccbb7a8c5
HlyanWintAung <hlyanwintaung@my.jcu.edu.au>
2024-06-08 18:01:02 +0800

    write min length and length and special_characters def main and print all the require

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -2,3 +2,11 @@ MIN_LENGTH = 2
 MAX_LENGTH = 6
 IS_SPECIAL_CHARACTER_REQUIRED = False
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
+
+
+def main():
+    print("Please enter a valid password")
+    print("Your password must be between 5 and 15 characters, and contain:")
+    print("\t1 or more uppercase characters")
+    print("\t1 or more lowercase characters")
+    print("\t1 or more numbers")
