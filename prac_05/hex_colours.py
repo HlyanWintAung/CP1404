f4300f85ccc830efd5633a34213a1e22c7eef97b
HlyanWintAung <hlyanwintaung@my.jcu.edu.au>
2024-06-22 21:48:47 +0800

    chose the 10 colours form link and ask the name use the while loop and done added the print colours

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,5 +1,7 @@
 COLOURS = {"cerise": "#de3163", "carolina blue": "#56a0d3", "carrot orange": "#ed9121", "castleton green": "#00563f", "cedar chest": "#c95a49", "celadon": "#ace1af", "celeste": "#b2ffff", "celtic blue": "#246bce", "cerulean frost": "#6d9bc3", "cg blue": "	#007aa5"}
+print(COLOURS)
 colour_name = input("Enter a colour name: ").lower()
 while colour_name != "":
     print(f"The code for \"{colour_name}\" is {COLOURS.get(colour_name)}")
     colour_name = input("Enter a colour name: ").lower()
+
