870947ac49b319b3f2ae8235b89141fc109d3aac
HlyanWintAung <hlyanwintaung@my.jcu.edu.au>
2024-07-05 23:41:46 +0800

    also write the choice and with exactly output i wrote the code with lower () and use it and also wrte more started

--- Assignment/a1_Books.py
+++ Assignment/a1_Books.py
@@ -1,3 +1,10 @@
+"""
+Assignment 1 for CP1404/CP5632, IT@JCU
+
+By:Hlyan Wint Aung
+"""
+
+
 import csv
 
 FILENAME = 'books.csv'
@@ -17,6 +24,18 @@ def main():
 
     while True:
         choice = input(MENU).strip().lower()
-
+        if choice == 'd':
+            print("")
+            display_books(books)
+        elif choice == 'a':
+            add_book(books)
+        elif choice == 'c':
+            complete_book(books)
+        elif choice == 'q':
+            save_books(books)
+            print("Books saved to books.csv\n\"So many books, so little time. Frank Zappa\"")
+            break
+        else:
+            print("Invalid menu choice")
 
 
